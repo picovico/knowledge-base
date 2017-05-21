77e9e694fc83625993b3d06e9eec5d1414c5d991
acpmasquerade <dhrubaster@gmail.com>
2017-05-21 19:03:52 +0545

    permalink

--- js/app.js
+++ js/app.js
@@ -33,13 +33,41 @@
 //     });
 // });
 
-function handle_category_click(self, data){
+PERMALINK = {
+    'category': null, 
+    'folder': null,
+    'article': null,
+    'init': function(){
+        var hash = window.location.hash;
+        var self = this;
+        if(hash && hash.startsWith("#!")){
+            hash = hash.replace("#!/", "").split("/");
+            self.category = hash[0];
+            self.folder = hash[1];
+            self.article = hash[2];
+        }
+    }, 
+    'traverse': function(handler){
+        var self = this;
+        if(self[handler]){
+            console.log(handler, self[handler]);
+            window.setTimeout(function(){
+                $("a[data-id=" + self[handler]+"]").click();
+                self[handler] = null;
+            }, 100);
+        }
+    }
+}
+
+function handle_category_click(self, data, parent_id, parent_permalink){
     // $(self).parent().parent().append("<hr />");
     var container = $("<div class='list-group folders content' style='display:none'></div>");
     $.each(data, function(index, item){
-        var anchor_dom = $("<a href='javascript:void()' class='folder api' data-parent=2></a>");
+        var anchor_dom = $("<a href='#' class='folder api' data-parent=2></a>");
         anchor_dom.attr("data-id", item.id);
+        anchor_dom.attr("data-parent-id", parent_id);
         anchor_dom.attr("data-handler", "folder");
+        anchor_dom.attr("data-permalink", parent_permalink + "/" + item.id);
         anchor_dom.attr("data-api", "solutions/folders/" + item.id + "/articles")
         anchor_dom.append(item.name);
         var heading_dom = $("<h4 class='collapsed'></h4>");
@@ -50,14 +78,17 @@ function handle_category_click(self, data){
     }) ;          
     $(self).parent().parent().parent().append(container);
     $(self).parent().parent().parent().find(".folders").fadeIn();
+    PERMALINK.traverse('folder');
 }
 
-function handle_folder_click(self, data){
+function handle_folder_click(self, data, parent_id, parent_permalink){
     var panel = $("<div class='panel content'></div>");
     var container = $("<div class='list-group articles' style='display:none'></div>");
     $.each(data, function(index, item){
-        var anchor_dom = $("<a href='javascript:void()' class='article question api' data-parent=2></a>");
+        var anchor_dom = $("<a href='#' class='article question api' data-parent=2></a>");
         anchor_dom.attr("data-id", item.id);
+        anchor_dom.attr("data-parent-id", parent_id);
+        anchor_dom.attr("data-permalink", parent_permalink + "/" + item.id);
         anchor_dom.attr("data-handler", "article");
         anchor_dom.attr("data-api", "solutions/articles/" + item.id );
         anchor_dom.append(item.title);
@@ -70,22 +101,35 @@ function handle_folder_click(self, data){
     panel.append(container);
     $(self).parent().parent().append(panel);
     $(self).parent().parent().find(".articles").fadeIn();
+    PERMALINK.traverse('article');
 }
 
-function handle_article_click(self, data){
-    var panel = $("<p class='article content answer'></p>");
-    panel.append("<strong> Answer : </strong> <br /> " + data.description);
+function handle_article_click(self, data, parent_id, parent_permalink){
+    var panel = $("<div class='article content answer'></div>");
+    var permalink = $("<div><a class='permalink'><strong>Answer : </strong></a></div>");
+    permalink.find("a").attr("href", "#!" + parent_permalink);
+    panel.append(permalink);
+    panel.append(data.description);
     $(self).parent().parent().append(panel);
     // $(self).parent().parent().find(".articles").fadeIn();
+
+    $('html, body').animate({
+        scrollTop: $("[data-id=" + data.id + "]").offset().top - 100
+    }, 300);
 }
 
 $(function(){
     $("#fd-categories").on("click", "a.api", function(event){
         $(".jumbotron").addClass("cya");
-        // event.preventDefault();
+        event.preventDefault();
         var self = this;
         var api = $(this).data("api");
         var handler = $(self).data("handler") ;
+
+        var parent_id = $(this).data("id");
+        var parent_permalink = $(this).data("permalink");
+
+        window.location.hash = "#!" + parent_permalink;
 
         $(this).parent().toggleClass("expanded");
         $(this).parent().parent().toggleClass("expanded-parent");
@@ -105,7 +149,7 @@ $(function(){
 
         $.getJSON("api_cache/" + api + "/response.json", function(data){
             var handler_fn = "handle_" + handler + "_click";
-            window[handler_fn](self, data);
+            window[handler_fn](self, data, parent_id, parent_permalink);
         });
         // return false;
     });
@@ -113,14 +157,16 @@ $(function(){
 
 
 $(function(){
+    PERMALINK.init();
     $.getJSON("api_cache/solutions/categories/response.json", function(data){
         $.each(data, function(index, item){
             category_dom = $("<div class='panel-body'></div>");
 
             var anchor_dom = $("<h3 class='expandthis collapsed'><a class='category api exclusive' data-parent=3></a></h3>");
-            anchor_dom.find("a").attr("href", "#!category=" + item.id);
+            anchor_dom.find("a").attr("href", "#");
             anchor_dom.find("a").attr("data-handler", "category");
-            anchor_dom.find("a").attr("data-category-id", item.id);
+            anchor_dom.find("a").attr("data-id", item.id);
+            anchor_dom.find("a").attr("data-permalink", "/" + item.id);
             anchor_dom.find("a").attr("data-api", "solutions/categories/" + item.id + "/folders");
             anchor_dom.find("a").append(item.name);
 
@@ -132,5 +178,6 @@ $(function(){
 
             $("#fd-categories").append(panel_dom);
         });
+        PERMALINK.traverse('category');
     });
 });
